b180c568f4dfb08bd7a4a2f15175b3ccb87fca09
Louis A <lakerm01@dcs.bbk.ac.uk>
2018-03-05 15:33:04 +0000

    adding code to machine and subclasses refactoring

--- courseworks/coursework-one-kotlin/src/sml/instructions/OutInstruction.kt
+++ courseworks/coursework-one-kotlin/src/sml/instructions/OutInstruction.kt
@@ -5,13 +5,8 @@ import sml.Machine
 
 class OutInstruction(label: String, val op: Int) : Instruction(label, "out") {
 
-    val value: Int = 0
-
     override fun execute(m: Machine) {
         val value = m.registers.getRegister(op)
-    }
-
-    override fun toString(): String {
-        return super.toString() + " register " + op + " contains " + value
+        println(this.label + ": register " + op + " value is " + value)
     }
 }
