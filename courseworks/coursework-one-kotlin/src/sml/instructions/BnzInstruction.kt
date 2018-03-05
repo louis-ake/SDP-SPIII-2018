39bcf1a2498a300e67889dccb774e4922c371ac9
Louis A <lakerm01@dcs.bbk.ac.uk>
2018-03-05 19:01:56 +0000

    adding code to machine and subclasses refactoring

--- courseworks/coursework-one-kotlin/src/sml/instructions/BnzInstruction.kt
+++ courseworks/coursework-one-kotlin/src/sml/instructions/BnzInstruction.kt
@@ -1,19 +1,22 @@
 package sml.instructions
 
 import sml.Instruction
+import sml.Labels
 import sml.Machine
 
 class BnzInstruction(label: String, val op: Int, label2: String) : Instruction(label, "bnz") {
 
-    val inst = label2
+    val inst: String = label2
 
     override fun execute(m: Machine) {
         println(m.prog)
         println(this.label + inst)
         val value = m.registers.getRegister(op)
         if (value != 0) {
-            m.prog.add(m.getInstruction(inst))
+            val ins = m.prog.indexOf(m.getInstruction(inst))
+            println("ins = " + ins)
+            val ins2 = m.prog[ins]
+            ins2.execute(m)
         }
-        println(m.prog)
     }
 }
