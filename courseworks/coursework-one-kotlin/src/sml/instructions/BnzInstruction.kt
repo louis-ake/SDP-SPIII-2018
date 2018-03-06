01b99836fdb6eae85c822b2d96916a0d9011efee
Louis Akerman <lakerm01@dcs.bbk.ac.uk>
2018-03-06 18:39:26 +0000

    toString function added to bnz

--- courseworks/coursework-one-kotlin/src/sml/instructions/BnzInstruction.kt
+++ courseworks/coursework-one-kotlin/src/sml/instructions/BnzInstruction.kt
@@ -7,12 +7,12 @@ import sml.Machine
 class BnzInstruction(label: String, val op: Int, val label2: String) : Instruction(label, "bnz") {
 
     override fun execute(m: Machine) {
-        println(m.prog)
-        println(this.label + label2)
         val value = m.registers.getRegister(op)
         val labels = m.labels.getLabels()
-        if (value != 0) {
-            m.pc = labels.indexOf(label2)
-        }
+        if (value != 0) { m.pc = labels.indexOf(label2) }
+    }
+
+    override fun toString(): String {
+        return super.toString() + " executing instruction " + label2 + " if the contents of register " + op + " is not 0"
     }
 }
