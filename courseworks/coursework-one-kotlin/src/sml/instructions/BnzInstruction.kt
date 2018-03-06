d0ef0e85f1cb3bbb6885eddfab0650b759304650
Louis Akerman <lakerm01@dcs.bbk.ac.uk>
2018-03-06 18:18:53 +0000

    all subclasses working

--- courseworks/coursework-one-kotlin/src/sml/instructions/BnzInstruction.kt
+++ courseworks/coursework-one-kotlin/src/sml/instructions/BnzInstruction.kt
@@ -4,19 +4,15 @@ import sml.Instruction
 import sml.Labels
 import sml.Machine
 
-class BnzInstruction(label: String, val op: Int, label2: String) : Instruction(label, "bnz") {
-
-    val inst: String = label2
+class BnzInstruction(label: String, val op: Int, val label2: String) : Instruction(label, "bnz") {
 
     override fun execute(m: Machine) {
         println(m.prog)
-        println(this.label + inst)
+        println(this.label + label2)
         val value = m.registers.getRegister(op)
+        val labels = m.labels.getLabels()
         if (value != 0) {
-            val ins = m.prog.indexOf(m.getInstruction(inst))
-            println("ins = " + ins)
-            val ins2 = m.prog[ins]
-            ins2.execute(m)
+            m.pc = labels.indexOf(label2)
         }
     }
 }
